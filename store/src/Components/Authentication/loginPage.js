ff2f64a4b8f3f29a37afba24638f165b828d5ff9
teju <tejuacfdredrfgvb@gmail.com>
2023-08-31 23:30:31 +0530

    teju changes

--- store/src/Components/Authentication/loginPage.js
+++ store/src/Components/Authentication/loginPage.js
@@ -9,7 +9,7 @@ export default function LoginPage (){
 <img src="https://images.unsplash.com/photo-1566888596782-c7f41cc184c5?ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80" className="img-fluid" style={{minHeight:"100%"}} />
 </div>
 <div className="col-md-6 bg-white p-5">
-<h3 className="pb-3">Login Form</h3>
+<h3 className="pb-3"> teju Login form</h3>
 <div className="form-style">
 <form>
 <div className="form-group pb-3">
