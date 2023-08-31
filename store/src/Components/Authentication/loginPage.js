f346ed4dab5d3b4fe4d9e9c9bed58190cb96a5ef
tejaswini <tejualapati2205@gmail.com>
2023-08-31 23:41:10 +0530

    name changes

--- store/src/Components/Authentication/loginPage.js
+++ store/src/Components/Authentication/loginPage.js
@@ -4,12 +4,13 @@ import React from 'react'
 export default function LoginPage (){
     return(
 <div className="container">
+<h3 > teju Login form</h3>
 <div className="row m-5 no-gutters shadow-lg">
 <div className="col-md-6 d-none d-md-block">
 <img src="https://images.unsplash.com/photo-1566888596782-c7f41cc184c5?ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80" className="img-fluid" style={{minHeight:"100%"}} />
 </div>
 <div className="col-md-6 bg-white p-5">
-<h3 className="pb-3"> teju Login form</h3>
+
 <div className="form-style">
 <form>
 <div className="form-group pb-3">
